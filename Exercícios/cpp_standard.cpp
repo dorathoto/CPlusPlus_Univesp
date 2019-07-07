b4e89e3d76f267764868f62b4ac60e2e10efb83d
Leonardo <leonardo.dorathoto@gmail.com>
2019-07-07 11:30:23 -0300

    tipos __cpluscplus

--- Exercícios/cpp_standard.cpp
+++ Exercícios/cpp_standard.cpp
@@ -6,9 +6,13 @@ int main()
  * Standards: c++89, c++03, c++11, c++14, c++17, etc.
  * No momento da compilação, podemos forçar um standard
  * com a opção -std=c++11, substituindo o standard desejado.
+ * 
+ *  199711 for C++98,
+    201103 for C++11
+    201402 for C++14
+    201703 for C++17
  */
 {
-    std::cout << "Standard: " <<  __cplusplus << std::endl;
+    std::cout << "Standard: " << __cplusplus << std::endl;
     return 0;
 }
-
